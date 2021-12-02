287977b818bdab3d594d6584a603213bf88bb07c
brunoczim <brunoczim@gmail.com>
2021-12-02 13:31:50 -0300

    add a technique for dynamic parser with clone

--- src/parser.rs
+++ src/parser.rs
@@ -8,19 +8,22 @@ pub enum Transition<P, T> {
 
 pub use Transition::*;
 
-pub trait Parser<I>: Sized {
+pub trait Parser<I> {
     type Output;
     type Error;
 
     fn transit(
         self,
         input: I,
-    ) -> Result<Transition<Self, Self::Output>, Self::Error>;
+    ) -> Result<Transition<Self, Self::Output>, Self::Error>
+    where
+        Self: Sized;
 
     fn map<F, T>(self, mapper: F) -> Map<Self, F>
     where
         I: Clone,
         F: FnOnce(Self::Output) -> T,
+        Self: Sized,
     {
         Map { inner: self, mapper }
     }
@@ -29,6 +32,7 @@ pub trait Parser<I>: Sized {
     where
         I: Clone,
         F: FnOnce(Self::Error) -> E,
+        Self: Sized,
     {
         MapErr { inner: self, mapper }
     }
@@ -37,6 +41,7 @@ pub trait Parser<I>: Sized {
     where
         I: Clone,
         F: FnOnce(Result<Self::Output, Self::Error>) -> Result<T, E>,
+        Self: Sized,
     {
         FilterMap { inner: self, mapper }
     }
@@ -45,6 +50,7 @@ pub trait Parser<I>: Sized {
     where
         I: Clone,
         E: From<Self::Error>,
+        Self: Sized,
     {
         ErrInto { inner: self, _marker: PhantomData }
     }
@@ -52,6 +58,7 @@ pub trait Parser<I>: Sized {
     fn map_input<F, J>(self, mapper: F) -> MapInput<Self, F>
     where
         F: FnMut(J) -> I,
+        Self: Sized,
     {
         MapInput { inner: self, mapper }
     }
@@ -60,6 +67,7 @@ pub trait Parser<I>: Sized {
     where
         I: Clone,
         Q: Parser<I, Output = Self::Output, Error = Self::Error>,
+        Self: Sized,
     {
         Or { state: OrState::Both(self, other) }
     }
@@ -68,6 +76,7 @@ pub trait Parser<I>: Sized {
     where
         I: Clone,
         Q: Parser<I, Error = Self::Error>,
+        Self: Sized,
     {
         And { left: Parsing(self), right: Parsing(other) }
     }
@@ -76,8 +85,90 @@ pub trait Parser<I>: Sized {
     where
         I: Clone,
         Q: Parser<I, Error = Self::Error>,
+        Self: Sized,
     {
         Then { state: ThenState::ParseLeft { left: self, right: second } }
+    }
+}
+
+impl<T, I> Parser<I> for Box<T>
+where
+    T: Parser<I>,
+{
+    type Output = T::Output;
+    type Error = T::Error;
+
+    fn transit(
+        self,
+        input: I,
+    ) -> Result<Transition<Self, Self::Output>, Self::Error> {
+        match (*self).transit(input)? {
+            Done(item) => Ok(Done(item)),
+            Parsing(parser) => Ok(Parsing(Box::new(parser))),
+        }
+    }
+}
+
+pub trait BoxedParser<'obj, I>: Parser<I> {
+    fn transit_boxed(
+        self: Box<Self>,
+        input: I,
+    ) -> Result<
+        Transition<DynParser<'obj, I, Self::Output, Self::Error>, Self::Output>,
+        Self::Error,
+    >;
+
+    fn dyn_clone(&self) -> DynParser<'obj, I, Self::Output, Self::Error>;
+}
+
+impl<'obj, T, I> BoxedParser<'obj, I> for T
+where
+    T: Parser<I> + 'obj + Send + Sync + Clone,
+{
+    fn transit_boxed(
+        self: Box<Self>,
+        input: I,
+    ) -> Result<
+        Transition<DynParser<'obj, I, Self::Output, Self::Error>, Self::Output>,
+        Self::Error,
+    > {
+        match (*self).transit(input)? {
+            Done(item) => Ok(Done(item)),
+            Parsing(parser) => Ok(Parsing(Box::new(parser))),
+        }
+    }
+
+    fn dyn_clone(&self) -> DynParser<'obj, I, Self::Output, Self::Error> {
+        Box::new(self.clone())
+    }
+}
+
+pub type DynParser<'obj, I, O, E> =
+    Box<dyn BoxedParser<'obj, I, Output = O, Error = E> + 'obj + Send + Sync>;
+
+impl<'obj, I, O, E> Parser<I> for DynParser<'obj, I, O, E> {
+    type Output = O;
+    type Error = E;
+
+    fn transit(
+        self,
+        input: I,
+    ) -> Result<Transition<Self, Self::Output>, Self::Error>
+    where
+        Self: Sized,
+    {
+        <dyn BoxedParser<I, Output = O, Error = E>>::transit_boxed(self, input)
+    }
+}
+
+impl<'obj, I, O, E> Clone for DynParser<'obj, I, O, E>
+where
+    I: 'obj,
+    O: 'obj,
+    E: 'obj,
+{
+    fn clone(&self) -> Self {
+        self.dyn_clone()
     }
 }
 
